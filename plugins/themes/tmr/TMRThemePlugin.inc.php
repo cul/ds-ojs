501e736e49959e0f0bc9bf5ad04bd5d4bc40acfd
Ed Madrid <edmadrid@gmail.com>
2019-04-23 12:16:20 -0400

    add Morningside Review child theme

---
+++ plugins/themes/tmr/TMRThemePlugin.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file plugins/themes/jcept-child/JCEPTChildThemePlugin.inc.php
+ *
+ * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2003-2016 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class TMRThemePlugin
+ * @ingroup plugins_themes_default
+ *
+ * @brief Default theme
+ */
+import('lib.pkp.classes.plugins.ThemePlugin');
+
+class TMRThemePlugin extends ThemePlugin {
+	/**
+	 * Initialize the theme's styles, scripts and hooks. This is only run for
+	 * the currently active theme.
+	 *
+	 * @return null
+	 */
+	public function init() {
+		$this->setParent('bootstrapthreechildthemeplugin');
+	}
+
+	/**
+	 * Get the display name of this plugin
+	 * @return string
+	 */
+	function getDisplayName() {
+		return __('plugins.themes.tmr.name');
+	}
+
+	/**
+	 * Get the description of this plugin
+	 * @return string
+	 */
+	function getDescription() {
+		return __('plugins.themes.tmr.description');
+	}
+}
+
+?>
---
+++ plugins/themes/tmr/readme.md
@@ -0,0 +1,3 @@
+# TMR Child Theme
+
+This theme child theme is based on the Bootstrap 3 child theme.
