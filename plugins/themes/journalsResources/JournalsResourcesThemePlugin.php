db123b1aaecc85520f904b60f46d3c27887d9103
Ed Madrid <edmadrid@gmail.com>
2026-06-02 11:52:45 -0400

    remove admin menu customizations from journalsHome and journalsResources themes

--- plugins/themes/journalsResources/JournalsResourcesThemePlugin.php
+++ plugins/themes/journalsResources/JournalsResourcesThemePlugin.php
@@ -9,7 +9,6 @@ class JournalsResourcesThemePlugin extends ThemePlugin {
 	public function init() {
 		$this->setParent('defaultthemeplugin');
 		$this->addStyle('custom', 'styles/custom.less');
-		$this->addStyle('custom-backend', 'styles/backend.less', array('contexts' => 'backend'));
 	}
 
 	public function getDisplayName() {
